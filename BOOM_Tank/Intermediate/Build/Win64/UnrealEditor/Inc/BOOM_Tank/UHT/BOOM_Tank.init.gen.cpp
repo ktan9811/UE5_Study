d70f4ba857d5b523c213d8f763271720f40c20d8
ktan9811 <ktan9811@gmail.com>
2023-12-21 17:32:46 +0900

    add default level map, create base_pawn class

--- BOOM_Tank/Intermediate/Build/Win64/UnrealEditor/Inc/BOOM_Tank/UHT/BOOM_Tank.init.gen.cpp
+++ BOOM_Tank/Intermediate/Build/Win64/UnrealEditor/Inc/BOOM_Tank/UHT/BOOM_Tank.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeBOOM_Tank_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x22984183,
-				0x2AF11CE7,
+				0x1F2B8D5B,
+				0x2BC0CE3B,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_BOOM_Tank.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_BOOM_Tank.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_BOOM_Tank(Z_Construct_UPackage__Script_BOOM_Tank, TEXT("/Script/BOOM_Tank"), Z_Registration_Info_UPackage__Script_BOOM_Tank, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x22984183, 0x2AF11CE7));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_BOOM_Tank(Z_Construct_UPackage__Script_BOOM_Tank, TEXT("/Script/BOOM_Tank"), Z_Registration_Info_UPackage__Script_BOOM_Tank, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x1F2B8D5B, 0x2BC0CE3B));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ BOOM_Tank/Intermediate/Build/Win64/UnrealEditor/Inc/BOOM_Tank/UHT/BasePawn.gen.cpp
@@ -0,0 +1,89 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "BOOM_Tank/BasePawn.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeBasePawn() {}
+// Cross Module References
+	BOOM_TANK_API UClass* Z_Construct_UClass_ABasePawn();
+	BOOM_TANK_API UClass* Z_Construct_UClass_ABasePawn_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_APawn();
+	UPackage* Z_Construct_UPackage__Script_BOOM_Tank();
+// End Cross Module References
+	void ABasePawn::StaticRegisterNativesABasePawn()
+	{
+	}
+	IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(ABasePawn);
+	UClass* Z_Construct_UClass_ABasePawn_NoRegister()
+	{
+		return ABasePawn::StaticClass();
+	}
+	struct Z_Construct_UClass_ABasePawn_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UECodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_ABasePawn_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_APawn,
+		(UObject* (*)())Z_Construct_UPackage__Script_BOOM_Tank,
+	};
+	static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_ABasePawn_Statics::DependentSingletons) < 16);
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ABasePawn_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Navigation" },
+		{ "IncludePath", "BasePawn.h" },
+		{ "ModuleRelativePath", "BasePawn.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_ABasePawn_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<ABasePawn>::IsAbstract,
+	};
+	const UECodeGen_Private::FClassParams Z_Construct_UClass_ABasePawn_Statics::ClassParams = {
+		&ABasePawn::StaticClass,
+		"Game",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009000A4u,
+		METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_ABasePawn_Statics::Class_MetaDataParams), Z_Construct_UClass_ABasePawn_Statics::Class_MetaDataParams)
+	};
+	UClass* Z_Construct_UClass_ABasePawn()
+	{
+		if (!Z_Registration_Info_UClass_ABasePawn.OuterSingleton)
+		{
+			UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_ABasePawn.OuterSingleton, Z_Construct_UClass_ABasePawn_Statics::ClassParams);
+		}
+		return Z_Registration_Info_UClass_ABasePawn.OuterSingleton;
+	}
+	template<> BOOM_TANK_API UClass* StaticClass<ABasePawn>()
+	{
+		return ABasePawn::StaticClass();
+	}
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ABasePawn);
+	ABasePawn::~ABasePawn() {}
+	struct Z_CompiledInDeferFile_FID_Unreal_Project_UE5_Study_BOOM_Tank_Source_BOOM_Tank_BasePawn_h_Statics
+	{
+		static const FClassRegisterCompiledInInfo ClassInfo[];
+	};
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Unreal_Project_UE5_Study_BOOM_Tank_Source_BOOM_Tank_BasePawn_h_Statics::ClassInfo[] = {
+		{ Z_Construct_UClass_ABasePawn, ABasePawn::StaticClass, TEXT("ABasePawn"), &Z_Registration_Info_UClass_ABasePawn, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(ABasePawn), 1698971196U) },
+	};
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Unreal_Project_UE5_Study_BOOM_Tank_Source_BOOM_Tank_BasePawn_h_3641485004(TEXT("/Script/BOOM_Tank"),
+		Z_CompiledInDeferFile_FID_Unreal_Project_UE5_Study_BOOM_Tank_Source_BOOM_Tank_BasePawn_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Unreal_Project_UE5_Study_BOOM_Tank_Source_BOOM_Tank_BasePawn_h_Statics::ClassInfo),
+		nullptr, 0,
+		nullptr, 0);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
